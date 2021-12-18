e2a38e1dc5a3f2f8d3f69ce8c795991275a17642
Samuel Vidovich <samuel.vidovich@gmail.com>
2021-12-18 18:05:01 -0500

    Clustering works, but is kinda slow and there are a lot more clusters than I think we need...

--- README.md
+++ README.md
@@ -9,3 +9,19 @@ We require:
 - Scikit Learn, for clustering
 
 We use numpy. Since it's a dependency of scikit-learn, we're not specifying it; we're going to use the version that comes with our pinned sklearn version.
+
+On Raspberry Pi, we ran into the error
+
+```
+Original error was: libf77blas.so.3: cannot open shared object file: No such file or directory
+```
+
+So we did the following:
+
+```
+sudo apt-get install libatlas-base-dev
+```
+
+The numpy developer documentation recommended either doing that or installing the version of numpy packaged for raspbian. Since we want to use the version of numpy included with sklearn for the least number of dependency headaches, we install libatlas instead.
+
+**If you run into additional issues running the script, please add an Issue with your problem or solution to this repository. If you don't have a solution, I'll do my best to come up with one.**
--- img2palette.py
+++ img2palette.py
@@ -1,8 +1,12 @@
 import argparse
+import numpy
 import os
+import time
 
+from numpy import ndarray
 from PIL import Image
-
+from sklearn.cluster import DBSCAN
+from typing import List, Tuple
 
 def read_image(path: str):
 
@@ -14,11 +18,36 @@ def read_image(path: str):
     height = size[1]
 
     print(f'Image mode is {image.mode}; image size is {image.size}')
-    pixels = list()
+    pixels: List[Tuple] = list()
     for x in range(width):
         for y in range(height):
             pixels.append(image.getpixel((x, y)))
     print(f'Acquired {len(pixels)} pixels!')
+    print('Packing pixels...')
+    numpy_pixel_data: ndarray = numpy.array(pixels)
+
+
+    print('Initiating model...')
+    # Estimate clustering structure from vector array.
+    # OPTICS (Ordering Points To Identify the Clustering Structure), closely related to DBSCAN, 
+    # finds core sample of high density and expands clusters from them [1]_. Unlike DBSCAN, keeps 
+    # cluster hierarchy for a variable neighborhood radius. Better suited for usage on large datasets 
+    # than the current sklearn implementation of DBSCAN.
+    # Author's note:
+    # min_samples chooses the minimum number of points around a vector to constitute a neighborhood.
+    # By choosing a high value, we're hoping to have larger, but fewer, clusters.
+    # eps defines the distance required to constitute 'in the neighborhood'. Since RGB colors are
+    # in the interval [0, 255], let's try out ~1% of 255, based on a 'why not' kind of philosophy. 
+    # We're choosing a Euclidean distance to keep things simple and to keep my brain thoughts thinking good.
+    # Going Minkowski ( or god forbid something more complex ) has a lot of implications that I don't have a 
+    # great intuition for, but Euclidean distances make sense in my smooth, caveman brain.
+    model = DBSCAN(min_samples=15, eps=2.55)
+    print('Fitting ( this may take some time )...')
+    t0 = time.time()
+    output_dataset: ndarray = model.fit_predict(numpy_pixel_data)
+    t1 = time.time()
+    print(f'Fit data in {t1-t0} seconds.')
+    print(f'Found {len(set(model.labels_))} clusters; labels are of type {type(model.labels_)}')
 
     
 
