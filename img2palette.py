102dfe3fed3e6ca09c2d1fb975735ddc62d635ec
Samuel Vidovich <samuel.vidovich@gmail.com>
2021-12-18 16:42:48 -0500

    Dirty error handling

--- img2palette.py
+++ img2palette.py
@@ -1,9 +1,11 @@
 import argparse
+import os
 
 from PIL import Image
 
 
 def read_image(path: str):
+
     image = Image.open(path)
     image.show()
 
@@ -12,6 +14,10 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input-file', required=True, help='The image to read')
     args = parser.parse_args()
+    input_file = args.input_file
+    if not os.path.isfile(input_file):
+        print(f'No such file {input_file}, exiting.')
+        return
 
     read_image(args.input_file)
 
